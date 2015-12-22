f249b3fa040939ce6f2281e008cb3958f1cc76b7
Sam Anderson <sam@lime-lab.com>
2015-12-22 14:36:00 -0800

    updated run template with openvpn's up/down scripts.  Also, added "interact" to the end, which seems to fix problems with authentication.

--- scripts/start_transmission.sh
+++ scripts/start_transmission.sh
@@ -2,5 +2,4 @@
 
 /usr/local/etc/rc.d/transmission stop
 python2.7 fix_ip.py "/usr/pbi/transmission-amd64/etc/transmission/home/settings.json" "$4"
-/usr/local/libexec/openvpn-client.up
 /usr/local/etc/rc.d/transmission start
--- scripts/stop_transmission.sh
+++ scripts/stop_transmission.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 /usr/local/etc/rc.d/transmission stop
-/usr/local/libexec/openvpn-client.down
